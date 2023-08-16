a3e2000b599c083440ab645e08e583597d1f8f8b
Willy <guillecg13@gmail.com>
2023-08-16 10:40:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/composestates/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.composestates
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composestates.ui.theme.ComposeStatesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStatesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyStateExample()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyStateExample() {
+
+    var counter by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter += 1}) {
+            Text(text = "Pulsar")
+        }
+        Text(text = "He sido pulsado ${counter} veces")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyStateExamplePreview() {
+    MyStateExample()
+}
